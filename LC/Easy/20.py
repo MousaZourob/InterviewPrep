c859da65f1f887e91cc492ed6a190a8cbf2cbd39
Mousa Zourob <mousazourob@gmail.com>
2022-08-18 16:00:07 -0400

    isValid

---
+++ LC/Easy/20.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        valid_combs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        stack = []
+        
+        for c in s:
+            if c in valid_combs:
+                if stack and stack[-1] == valid_combs[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+            
+        return len(stack) == 0
