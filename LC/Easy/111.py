9f829a79a935b452623ccb2ef331d0a961689ffd
Mousa Zourob <mousazourob@gmail.com>
2022-08-20 16:53:30 -0400

    minDepth

---
+++ LC/Easy/111.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        q = collections.deque()
+        q.append(root)
+        
+        curr_depth = 0
+        while q:
+            level_size = len(q)
+            curr_depth += 1
+            
+            for _ in range(level_size):
+                curr = q.popleft()
+                if curr:
+                    if not curr.right and not curr.left:
+                        return curr_depth
+                    
+                    if curr.left:
+                        q.append(curr.left)
+                    if curr.right:
+                        q.append(curr.right)
+        
+        return 0
