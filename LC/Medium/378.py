a50d78628e74d58ac033899549394c60b5ca4881
Mousa Zourob <mousazourob@gmail.com>
2022-09-20 21:15:30 -0400

    kthSmallest

---
+++ LC/Medium/378.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        heap = []
+        
+        for row in matrix:
+            for num in row:
+                heappush(heap, -num)
+                
+                if len(heap) > k:
+                    heappop(heap)
+                    
+        return -heap[0]
