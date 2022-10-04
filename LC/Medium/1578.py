ad2691184a2c693f9c4582e98cc509f21ccb4b23
Mousa Zourob <mousazourob@gmail.com>
2022-10-03 20:47:41 -0400

    minCost

---
+++ LC/Medium/1578.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        total_time = 0
+        window_start = 0
+        max_time = neededTime[window_start]
+        
+        for window_end in range(1, len(colors)):
+            if colors[window_start] == colors[window_end]:
+                total_time += min(neededTime[window_start], neededTime[window_end])
+                
+                if neededTime[window_start] < neededTime[window_end]:
+                    window_start = window_end
+            else:
+                window_start = window_end
+            
+        return total_time
