65d7235f05bee8abc32e1b4ff48f957210beb3a3
Mousa Zourob <mousazourob@gmail.com>
2022-10-06 21:18:10 -0400

    findMinArrowShots

---
+++ LC/Medium/452.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key = lambda b: b[1])
+        ans = 0
+        curr_arrow = None
+        
+        for start, end in points:
+            if not curr_arrow or start > curr_arrow:
+                ans += 1
+                curr_arrow = end
+        
+        return ans
