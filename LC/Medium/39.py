a52290cc7336052c683f07d7786dc338d90cc831
Mousa Zourob <mousazourob@gmail.com>
2022-10-31 14:52:34 -0400

    combinationSum

--- LC/Medium/39.py
+++ LC/Medium/39.py
@@ -2,19 +2,20 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         
-        def dfs(i, curr, total):
+        subset = []
+        def dfs(i, total):
             if total == target:
-                ans.append(list(curr))
+                ans.append(list(subset))
                 return
             if i >= len(candidates) or total > target:
                 return
             
-            curr.append(candidates[i])
-            dfs(i, curr, total + candidates[i])
+            subset.append(candidates[i])
+            dfs(i, total + candidates[i])
             
-            curr.pop()
-            dfs(i+1, curr, total)
+            subset.pop()
+            dfs(i+1, total)
             
-        dfs(0, [], 0)
+        dfs(0, 0)
         
         return ans
