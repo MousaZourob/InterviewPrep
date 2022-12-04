1d9ebe4d2feccbfaf70628b3d0805919bfe3d0c7
Mousa Zourob <mousazourob@gmail.com>
2022-12-03 19:16:00 -0500

    longestPalindrome

--- LC/Medium/5.py
+++ LC/Medium/5.py
@@ -1,19 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(start, left, right):
-            while (left >= 0 and right < len(s) and s[left] == s[right]):
-                left -= 1
-                right += 1
-            return right - left - 1
-
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return r - l - 1
+            
         ans_l, ans_r = 0, 0
         
         for i in range(len(s)):
-            ans = max(expand(ans_l, i, i), expand(ans_l, i, i+1))
+            curr = max(expand(i, i), expand(i, i+1))
             
-            if ans > ans_r - ans_l:
-                ans_l = i - (ans-1) // 2
-                ans_r = i + ans // 2
+            if curr > ans_r - ans_l:
+                ans_l = i - (curr - 1) // 2
+                ans_r = i + curr // 2
                 
         return s[ans_l:ans_r+1]
+    
     
