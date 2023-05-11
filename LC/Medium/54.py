88b90e4d718ede890edcf49812db51486b1bc4ee
Mousa Zourob <mousazourob@gmail.com>
2023-05-11 19:05:22 -0400

    spiralOrder

--- LC/Medium/54.py
+++ LC/Medium/54.py
@@ -3,37 +3,20 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
-        curr_dirr = "right"
-        l, r = 0, n - 1
-        t, b = 0, m - 1
+        i, j = 0, -1
+        dirr = 1
+        
         ans = []
         
-        counter = 0
-        while len(ans) < m*n:
-            if curr_dirr == "right":
-                for i in range(l, r+1):
-                    ans.append(matrix[t][i])
-                t += 1
-                curr_dirr = "down"
-                
-            elif curr_dirr == "down":
-                for i in range(t, b+1):
-                    ans.append(matrix[i][r])
-                r -= 1
-                curr_dirr = "left"
+        while 0 < m*n:
+            for _ in range(n):
+                j += dirr
+                ans.append(matrix[i][j])
+            m -= 1
+            for _ in range(m):
+                i += dirr
+                ans.append(matrix[i][j])
+            n -= 1
+            dirr *= -1
             
-            elif curr_dirr == "left":
-                for i in range(r, l-1, -1):
-                    ans.append(matrix[b][i])
-                b -= 1
-                curr_dirr = "up"
-            
-            elif curr_dirr == "up":
-                for i in range(b, t-1, -1):
-                    ans.append(matrix[i][l])
-                l += 1
-                curr_dirr = "right"
-            
-            counter += 1   
-        
         return ans
