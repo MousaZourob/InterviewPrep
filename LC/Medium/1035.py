f1ee3a4c605a76695745138fbf907b4ea42d5681
Mousa Zourob <mousazourob@gmail.com>
2023-05-12 17:09:19 -0400

    maxUncrossedLines

--- LC/Medium/1035.py
+++ LC/Medium/1035.py
@@ -3,14 +3,15 @@ class Solution:
         n1 = len(nums1)
         n2 = len(nums2)
 
-        dp = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+        prev = [0] * (n2 + 1)
+        dp = [0] * (n2 + 1)
 
         for i in range(1, n1+1):
             for j in range(1, n2+1):
                 if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    dp[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-                
-        return dp[n1][n2]
-        
+                    dp[j] = max(dp[j-1], prev[j])
+            prev = dp[:]
+
+        return dp[-1]
