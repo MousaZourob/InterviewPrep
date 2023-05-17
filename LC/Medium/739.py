60a1cca23a9fa258f526d7e0321dc6b00131d9dd
Mousa Zourob <mousazourob@gmail.com>
2023-05-17 18:50:31 -0400

    dailyTemperatures

--- LC/Medium/739.py
+++ LC/Medium/739.py
@@ -1,14 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        ans = [0]*n
+        ans = [0] * len(temperatures)
         stack = []
-
-        for curr_day in range(n):
-            while stack and temperatures[stack[-1]] < temperatures[curr_day]:
-                past_day = stack.pop()
-                ans[past_day] = curr_day - past_day
-
-            stack.append(curr_day)
-
+        
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                past = stack.pop()
+                ans[past] = i - past
+            
+            stack.append(i)
+            
         return ans
