c1b059b9afac6c7e2f601621416f9905e9ea1b5f
Mousa Zourob <mousazourob@gmail.com>
2023-05-21 15:31:36 -0400

    TimeMap

--- LC/Medium/981.py
+++ LC/Medium/981.py
@@ -1,25 +1,28 @@
-from sortedcontainers import SortedDict
-
 class TimeMap:
+
     def __init__(self):
-        self.data = {}
+        self.data = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if not key in self.data:
-            self.data[key] = SortedDict()
+        self.data[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str:       
+        arr = self.data[key]
         
-        self.data[key][timestamp] = value
+        l, r = 0, len(arr) - 1
+        res = ""
+        while l <= r:
+            m = (l+r)//2
             
-    def get(self, key: str, timestamp: int) -> str:
-        if not key in self.data:
-            return ""
+            if arr[m][0] <= timestamp:
+                res = arr[m][1]
+                l = m + 1
+            else:
+                r = m - 1
         
-        it = self.data[key].bisect_right(timestamp)
-        if it == 0:
-            return ""
-        
-        return self.data[key].peekitem(it - 1)[1]
-        
+        return res
+
+
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
