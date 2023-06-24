bdb09b2b2c3dff0d59a4a3cc272af60a0a60a67f
Mousa Zourob <mousazourob@gmail.com>
2023-06-24 19:43:15 -0400

    Trie

---
+++ LC/Medium/208.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self):
+        self.is_word = False
+        self.children = [None] * 26
+        
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        
+        for c in word:
+            i = ord(c) - ord('a')
+            if not curr.children[i]:
+                curr.children[i] = Node()
+            curr = curr.children[i]
+            
+        curr.is_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        
+        for c in word:
+            i = ord(c) - ord('a')            
+            if not curr.children[i]:
+                return False
+            curr = curr.children[i]
+            
+        return curr.is_word
+        
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        
+        for c in prefix:
+            i = ord(c) - ord('a')
+            if not curr.children[i]:
+                return False
+            curr = curr.children[i]
+            
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
