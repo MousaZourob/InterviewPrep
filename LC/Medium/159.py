93edfa653ca82db959f1e73dcf69ec385179b2fd
Mousa Zourob <66835262+MousaZourob@users.noreply.github.com>
2023-07-13 15:55:48 -0400

    lengthOfLongestSubstringTwoDistinct

--- LC/Medium/159.py
+++ LC/Medium/159.py
@@ -1,18 +1,18 @@
 class Solution:
-	def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-		count = defaultdict(int)
-		window_start = 0
-		ans = 0
-		
-		for window_end in range(len(s)):
-			count[s[window_end]] += 1
-		
-		while len(count) > 2:
-			count[s[window_start]] -= 1
-			if count[s[window_start]] == 0:
-				del count[s[window_start]]
-			window_start += 1    
-		
-		ans = max(ans, window_end - window_start + 1)
-		
-		return ans
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        count = defaultdict(int)
+        window_start = 0
+        ans = 0
+        
+        for window_end in range(len(s)):
+            count[s[window_end]] += 1
+            
+            while len(count) > 2:
+                count[s[window_start]] -= 1
+                if count[s[window_start]] == 0:
+                    del count[s[window_start]]
+                window_start += 1    
+            
+            ans = max(ans, window_end - window_start + 1)
+        
+        return ans
