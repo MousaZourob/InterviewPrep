5c8de6ba0e33af4d6c07bb754dd6b407530dad1f
Mousa Zourob <mousazourob@gmail.com>
2023-07-18 21:09:55 -0400

    canFinish

--- LC/Medium/207.py
+++ LC/Medium/207.py
@@ -1,26 +1,22 @@
 class Solution:
-    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        indegree = [0] * n
-        adj = defaultdict(list)
-
-        for i in range(n):
-            for node in graph[i]:
-                adj[node].append(i)
-                indegree[i] += 1
-
-        q = [i for i in range(n) if indegree[i] == 0]
-
+    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+        in_degree = [0] * n
         
-        safe = [False] * n
-        while q:
-            node = q.pop(0)
-            safe[node] = True
-            for neighbor in adj[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+        for parent, child in prerequisites:
+            adj_list[parent].append(child)
+            in_degree[child] += 1
+            
+        sources = [i for i in range(n) if in_degree[i] == 0]
         
-        safeNodes = [i for i in range(n) if safe[i]]
-
-        return safeNodes
+        courses = 0
+        while sources:
+            curr = sources.pop(0)
+            courses += 1
+            
+            for neighbour in adj_list[curr]:
+                in_degree[neighbour] -= 1
+                if in_degree[neighbour] == 0:
+                    sources.append(neighbour)
+        print(courses)
+        return courses == n
