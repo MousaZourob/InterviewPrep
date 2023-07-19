a17322e420f5d34bd9d04fd505266eab87890c9d
Mousa Zourob <mousazourob@gmail.com>
2023-07-18 21:03:26 -0400

    eventualSafeNodes

--- LC/Medium/207.py
+++ LC/Medium/207.py
@@ -1,28 +1,26 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        sorted_graph = []
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        indegree = [0] * n
+        adj = defaultdict(list)
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = [i for i in range(n) if indegree[i] == 0]
+
         
-        if numCourses < 1:
-            return False
+        safe = [False] * n
+        while q:
+            node = q.pop(0)
+            safe[node] = True
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
         
-        in_degree = {i: 0 for i in range(numCourses)}
-        graph =  defaultdict(list)
-        
-        for parent, child in prerequisites:
-            graph[parent].append(child)
-            in_degree[child] += 1
-            
-        sources = []
-        for key in in_degree:
-            if in_degree[key] == 0:
-                sources.append(key)
-        
-        while sources:
-            vertex = sources.pop(0)
-            sorted_graph.append(vertex)
-            for child in graph[vertex]:
-                in_degree[child] -= 1
-                if in_degree[child] == 0:
-                    sources.append(child)
-        
-        return len(sorted_graph) == numCourses
+        safeNodes = [i for i in range(n) if safe[i]]
+
+        return safeNodes
