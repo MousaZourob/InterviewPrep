3797585f559a058c99c51d2b42f01ccc0bca4506
Mousa Zourob <mousazourob@gmail.com>
2023-09-25 16:57:48 -0400

    isValidSudoku

--- LC/Medium/36.py
+++ LC/Medium/36.py
@@ -1,25 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxs = [set() for _ in range(9)]
+        rows = [[0] * 9 for _ in range(9)]
+        cols = [[0] * 9 for _ in range(9)]
+        boxs = [[0] * 9 for _ in range(9)]
         
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                   
-                if board[r][c] in rows[r]:
-                    return False
-                rows[r].add(board[r][c])
-                
-                if board[r][c] in cols[c]:
-                    return False
-                cols[c].add(board[r][c])
-                
-                idx = (r // 3) * 3 + c // 3
-                if board[r][c] in boxs[idx]:
-                    return False
-                boxs[idx].add(board[r][c])
-                
+                if board[r][c] != '.':
+                    val = int(board[r][c]) - 1
+
+                    if rows[r][val] == 1:
+                        return False
+                    rows[r][val] = 1
+
+                    if cols[c][val] == 1:
+                        return False
+                    cols[c][val] = 1
+                    
+                    box_num = (r//3) * 3 + (c//3)
+                    if boxs[box_num][val] == 1:
+                        return False
+                    boxs[box_num][val] = 1
+        
         return True
