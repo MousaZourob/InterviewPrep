5e577a183bccf83a6d7bd656bc6a713c708f54ef
Mousa Zourob <mousazourob@gmail.com>
2023-11-30 19:21:19 -0500

    arrayStringsAreEqual

--- LC/Easy/1662.py
+++ LC/Easy/1662.py
@@ -1,3 +1,12 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        return "".join(word1) == "".join(word2)
+        def generate(word):
+            for w in word:
+                for c in w:
+                    yield c
+            yield None
+            
+        for c1, c2 in zip(generate(word1), generate(word2)):
+            if c1 != c2:
+                return False
+        return True
