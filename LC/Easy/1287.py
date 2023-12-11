da9258b368ebfc348e3a70877e5c5a0147eb09c6
Mousa Zourob <mousazourob@gmail.com>
2023-12-11 10:40:29 -0500

    findSpecialInteger

---
+++ LC/Easy/1287.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+    
+        for i in range(n):
+            if arr[i] == arr[i + n // 4]:
+                return arr[i]
+        
+        return -1
