c4dc24d4faabb8dda625dbaa83373f61c5ec106a
Mousa Zourob <mousazourob@gmail.com>
2024-01-03 19:58:52 -0500

    maxProfit

--- LC/Medium/309.py
+++ LC/Medium/309.py
@@ -1,20 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
+        sold = -inf
+        held = -inf
+        reset = 0
         
-        def dfs(i, buy):
-            if i >= len(prices):
-                return 0
-            if (i, buy) in dp:
-                return dp[(i,buy)]
+        for price in prices:
+            sold, held, reset = held + price, max(held, reset - price), max(reset, sold)
             
-            skip = dfs(i+1, buy) 
-            if buy:
-                bought = dfs(i+1, not buy) - prices[i]
-                dp[(i, buy)] = max(skip, bought)
-            else:
-                sold = dfs(i+2, not buy) + prices[i]
-                dp[(i, buy)] = max(skip, sold)
-            return dp[(i, buy)]
-        
-        return dfs(0, True)
+        return max(sold, reset)
