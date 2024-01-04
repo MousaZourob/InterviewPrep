95238bcd498fa4a4bde78f733498ce7375ce8b89
Mousa Zourob <mousazourob@gmail.com>
2024-01-03 20:13:51 -0500

    change

--- LC/Medium/518.py
+++ LC/Medium/518.py
@@ -1,12 +1,11 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-        dp[0] = [1] * (len(coins) + 1)
+        n = len(coins)
+        dp = [0] * (amount + 1)
+        dp[0] = 1
         
-        for a in range(1, amount + 1):
-            for i in range(len(coins) - 1, -1, -1):
-                dp[a][i] = dp[a][i+1]
-                if a - coins[i] >= 0:
-                    dp[a][i] += dp[a-coins[i]][i]
-                    
-        return dp[amount][0]
+        for i in range(n - 1, -1, -1):
+            for j in range(coins[i], amount + 1):
+                dp[j] += dp[j - coins[i]]
+
+        return dp[amount]
