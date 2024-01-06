97ab7dc7f8779beba75e9fc5ff6ed0379866b7ba
Mousa Zourob <mousazourob@gmail.com>
2024-01-06 15:29:12 -0500

    jobScheduling

--- LC/Hard/1235.py
+++ LC/Hard/1235.py
@@ -1,19 +1,19 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[0])
-        n = len(profit)
-        startTime = [jobs[i][0] for i in range(n)]
-        dp = {}
+        n = len(startTime)
+        jobs = sorted(zip(startTime, endTime, profit), key = lambda x: x[0])
+        dp = [0] * n
+        start_times = [job[0] for job in jobs]
         
         def dfs(i):
             if i >= n:
                 return 0
-            if i in dp:
+            if dp[i]:
                 return dp[i]
             
-            j = bisect_left(startTime, jobs[i][1])
+            j = bisect_left(start_times, jobs[i][1])
+                        
             dp[i] = max(jobs[i][2] + dfs(j), dfs(i+1))
-            
             return dp[i]
-            
+        
         return dfs(0)
