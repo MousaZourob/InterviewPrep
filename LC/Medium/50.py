0dbcc83e80e550bfaea73349e9c2660e547e3c2d
Mousa Zourob <mousazourob@gmail.com>
2024-01-14 11:12:16 -0500

    myPow

--- LC/Medium/50.py
+++ LC/Medium/50.py
@@ -1,17 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         def divide(num, exp):
-            if num == 0:
-                return 0
+            if exp == 1:
+                return num
             if exp == 0:
                 return 1
+            if num == 0:
+                return 0
             
             res = divide(num, exp // 2)
             res *= res
             
             return num * res if exp % 2 else res 
             
-        ans = divide(x, n)
+        ans = divide(x, abs(n))
         if n >= 0:
             return ans
         else:
