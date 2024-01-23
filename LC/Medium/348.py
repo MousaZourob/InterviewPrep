4fdab70249694fc05b5af5080ce3d64db7ec68b0
Mousa Zourob <mousazourob@gmail.com>
2024-01-23 09:34:47 -0500

    TicTacToe

--- LC/Medium/348.py
+++ LC/Medium/348.py
@@ -2,33 +2,27 @@ class TicTacToe:
 
     def __init__(self, n: int):
         self.n = n
-        self.rows = [0] * n
-        self.cols = [0] * n
-        self.diag = [0] * 2
+        self.rows = [0]*n
+        self.cols = [0]*n
+        self.diagonal = 0
+        self.anti_diagonal = 0
 
     def move(self, row: int, col: int, player: int) -> int:
-        if player == 1:
-            self.rows[row] += 1
-            self.cols[col] += 1
+        p = 1 if player == 1 else -1
+        
+        self.rows[row] += p
+        self.cols[col] += p
+        
+        if row == col:
+            self.diagonal += p
+        
+        if row + col == self.n - 1:
+            self.anti_diagonal += p
             
-            if row == col:
-                self.diag[0] += 1
-            if row + col == self.n - 1:
-                self.diag[1] += 1
-            if self.rows[row] == self.n or self.cols[col] == self.n or self.diag[0] == self.n or self.diag[1] == self.n:
-                return 1
-        else:
-            self.rows[row] -= 1
-            self.cols[col] -= 1
-            
-            if row == col:
-                self.diag[0] -= 1
-            if row + col == self.n - 1:
-                self.diag[1] -= 1
-            if self.rows[row] == -self.n or self.cols[col] == -self.n or self.diag[0] == -self.n or self.diag[1] == -self.n:
-                return 2
-            
-        return 0
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diagonal) == self.n or abs(self.anti_diagonal) == self.n:
+            return player
+        
+        return 0    
 
 
 # Your TicTacToe object will be instantiated and called as such:
