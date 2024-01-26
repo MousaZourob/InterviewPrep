26d1bfc05f1e29f15d52248a55808450862f79ab
Mousa Zourob <mousazourob@gmail.com>
2024-01-26 07:33:41 -0500

    reachingPoints

---
+++ LC/Hard/780.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while tx >= sx + ty or ty >= sy + tx: 
+            if tx > ty:
+                tx = sx + (tx - sx) % ty
+            else:
+                ty = sy + (ty - sy) % tx
+
+        return tx == sx and ty == sy
