dd918e5de34ced790a5227d1e8ce5590a168b2bc
Mousa Zourob <mousazourob@gmail.com>
2024-02-14 17:39:06 -0500

    rearrangeArray

---
+++ LC/Medium/2149.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        ans = [0]*len(nums)
+        neg = 1
+        pos = 0
+        
+        for num in nums:
+            if num > 0:
+                ans[pos] = num
+                pos += 2
+            else:
+                ans[neg] = num
+                neg += 2
+            
+        return ans
+        
