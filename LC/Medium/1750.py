df70c797f7ec01e538bbc169d08cddc8e2a6e14f
Mousa Zourob <mousazourob@gmail.com>
2024-03-05 18:34:56 -0500

    minimumLength

---
+++ LC/Medium/1750.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        n = len(s)
+        l, r = 0, n - 1
+        
+        while l < r and s[l] == s[r]:
+            c = s[l]
+            
+            while l <= r and s[l] == c:
+                l += 1
+            while r > l and s[r] == c:
+                r -= 1
+            
+        return r - l + 1
