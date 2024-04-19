8e036c47af96f6e7c62aed3176f2590faf7f4cef
Mousa Zourob <mousazourob@gmail.com>
2024-04-19 17:39:34 -0400

    numIslands

--- LC/Medium/200.py
+++ LC/Medium/200.py
@@ -1,21 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ans = 0
+        _dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         m = len(grid)
         n = len(grid[0])
         
-        def dfs(row, col):
-            if 0 <= row < m and 0 <= col < n and grid[row][col] == "1":
-                grid[row][col] = "0"
-                dfs(row+1, col)
-                dfs(row-1, col)
-                dfs(row, col+1)
-                dfs(row, col-1)        
+        def dfs(x, y):
+            grid[x][y] = '0'
+            for r_x, r_y in _dir:
+                n_r, n_c = r_x + x, r_y + y
+                if 0 <= n_r < m and 0 <= n_c < n and grid[n_r][n_c] == '1':
+                    dfs(n_r, n_c)
+            
+        ans = 0
         
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    dfs(i, j)
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    dfs(r,c)
                     ans += 1
-        
         return ans
