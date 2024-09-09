39a98d5dcdf3560e07d3c2ee39a99b38a3ae573d
Mousa Zourob <mousazourob@gmail.com>
2024-09-09 08:35:43 -0400

    isToeplitzMatrix

--- LC/Easy/766.py
+++ LC/Easy/766.py
@@ -1,25 +1,11 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
         
-        for i in range(cols):
-            curr = matrix[rows-1][i]
-            j = 0
-            
-            while rows-1-j >= 0 and i-j >= 0:
-                if curr != matrix[rows-1-j][i-j]:
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] != matrix[i-1][j-1]:
                     return False
-                j += 1
-        
-        for i in range(rows):
-            curr = matrix[i][cols-1]
-            j = 0
-            
-            while cols-1-j >=0 and i-j >= 0:
-                if curr != matrix[i-j][cols-1-j]:
-                    return False
-                j+=1
-        
-        
+
         return True
