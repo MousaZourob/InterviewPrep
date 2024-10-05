c66be3eedf248cabfdf1c1b996d09ed783aa29e4
Mousa Zourob <mousazourob@gmail.com>
2024-10-05 12:39:19 -0400

    checkInclusion

--- LC/Medium/567.py
+++ LC/Medium/567.py
@@ -1,16 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if Counter(s1) == Counter(s2):
-            return True
+        count = Counter(s1)
         window_start = 0
         
+        curr = defaultdict(int)
         for window_end in range(len(s2)):
-            if window_end - window_start + 1 == len(s1):
-                if Counter(s1) == Counter(s2[window_start:window_end+1]):
-                    return True
+            curr[s2[window_end]] += 1
+            if curr == count:
+                return True
             
-            if window_end - window_start + 2 > len(s1):
+            while window_end - window_start + 2 > len(s1):
+                curr[s2[window_start]] -= 1
+                if curr[s2[window_start]] == 0:
+                    del curr[s2[window_start]]
                 window_start += 1
-            
-        return False
         
+        return False
