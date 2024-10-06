89f11b54a0e6a3638eb9268286f27f140939ed0b
Mousa Zourob <mousazourob@gmail.com>
2024-10-06 10:08:26 -0400

    areSentencesSimilar

---
+++ LC/Medium/1813.py
@@ -0,0 +1,14 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        q1 = deque(sentence1.split())
+        q2 = deque(sentence2.split())
+        
+        while q1 and q2 and q1[0] == q2[0]:
+            q1.popleft()
+            q2.popleft()
+    
+        while q1 and q2 and q1[-1] == q2[-1]: 
+            q1.pop()
+            q2.pop()
+        
+        return not q1 or not q2
