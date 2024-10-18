ffe1a4e9005b78aba53c982520921d86ec5c79aa
Mousa Zourob <mousazourob@gmail.com>
2024-10-18 12:46:36 -0400

    countMaxOrSubsets

---
+++ LC/Medium/2044.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+        max_or = 0
+        
+        for n in nums:
+            new_dp = deepcopy(dp)
+            for cur_or, count in dp.items():
+                new_or = n | cur_or
+                new_dp[new_or] += count
+            
+            dp = new_dp
+            max_or |= n
+        
+        return dp[max_or]
