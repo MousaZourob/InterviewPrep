5d7d47539bb231e5c9861a6ff2feb0770a9025e5
Mousa Zourob <mousazourob@gmail.com>
2024-11-10 17:37:41 -0500

    decodeString

---
+++ LC/Medium/394.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        n = len(s)
+        
+        for c in s:
+            if c != ']':
+                stack.append(c)
+            else:
+                word = ""
+                while stack and stack[-1] != '[':
+                    word = stack.pop() + word
+                stack.pop()
+    
+                count = ""
+                while stack and stack[-1].isdigit():
+                    count = stack.pop() + count
+
+                stack.append(int(count) * word)
+        
+        return ''.join(stack)
