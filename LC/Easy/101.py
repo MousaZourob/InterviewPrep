8de1a2638a6b5c2bbc906fb2f6e0dcd9c1eac591
Mousa Zourob <mousazourob@gmail.com>
2024-11-12 22:34:37 -0500

    isSymmetric

---
+++ LC/Easy/101.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(n1, n2):
+            if not n1 and not n2:
+                return True
+            if not n1 or not n2:
+                return False
+            
+            return n1.val == n2.val and dfs(n1.right, n2.left) and dfs(n1.left, n2.right)
+        
+        return dfs(root, root)
