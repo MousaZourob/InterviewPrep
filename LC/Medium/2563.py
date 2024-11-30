3977de4cce99363cf12711964f13a0f604e9985e
Mousa Zourob <mousazourob@gmail.com>
2024-11-30 15:58:08 -0500

    countFairPairs

--- LC/Medium/2563.py
+++ LC/Medium/2563.py
@@ -2,12 +2,21 @@ class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         n = len(nums)
         nums.sort()
-        ans = 0
-        
-        for i, num in enumerate(nums):
-            low = bisect_left(nums, lower - nums[i], i + 1)
-            high = bisect_right(nums, upper - nums[i], i + 1)
+        def bin_search(nums, l, r, target):
+            while l <= r:
+                m = l + (r - l) // 2
+                
+                if nums[m] >= target:
+                    r = m - 1
+                else:
+                    l = m + 1
             
+            return l
+        
+        ans = 0
+        for i, num in enumerate(nums):
+            low = bin_search(nums, i + 1, n - 1, lower - num)
+            high = bin_search(nums, i + 1, n - 1, upper - num + 1)
             ans += high - low
             
         return ans
