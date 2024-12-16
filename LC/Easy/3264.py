e423295daa5188dec228b6a50d8df74578a491e6
Mousa Zourob <mousazourob@gmail.com>
2024-12-16 09:19:52 -0500

    getFinalState

---
+++ LC/Easy/3264.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        heap = [(num, i) for i, num in enumerate(nums)]
+        heapify(heap)
+        
+        for _ in range(k):
+            _, i = heappop(heap)
+            nums[i] *= multiplier
+            heappush(heap, (nums[i], i))
+        
+        return nums
