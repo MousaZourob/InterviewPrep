442d330599b888334efe678d5a965d8545cc229a
Mousa Zourob <mousazourob@gmail.com>
2025-06-13 17:25:00 -0400

    minimizeMax

--- LC/Python/Medium/2616.py
+++ LC/Python/Medium/2616.py
@@ -1,32 +1,32 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
         nums.sort()
-        ans = 0
         n = len(nums)
-        
-        def countPairs(guess):
+        ans = 0
+
+        def count_pairs(guess):
             i, c = 0, 0
-                        
             while i < n - 1:
                 if nums[i + 1] - nums[i] <= guess:
                     i += 2
                     c += 1
-                else:
+                else: 
                     i += 1
                 
                 if c >= p:
                     return True
-            
+
             return False
-        
-        l, r = 0, nums[-1] - nums[0]
+
+        l = 0
+        r = nums[-1] - nums[0]
         while l <= r:
-            m = l + (r-l) // 2
-            
-            if countPairs(m):
+            m = l + (r - l) // 2
+
+            if count_pairs(m):
                 ans = m
                 r = m - 1
             else:
                 l = m + 1
-        
+
         return ans
