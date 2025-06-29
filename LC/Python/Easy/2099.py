20d2160eb54c7b2bca0dc311c361030c82f6cfaa
Mousa Zourob <mousazourob@gmail.com>
2025-06-28 22:11:03 -0400

    maxSubsequence

---
+++ LC/Python/Easy/2099.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        for i, n in enumerate(nums):
+            heapq.heappush(heap, (n, i))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [x[0] for x in sorted(heap, key=lambda x: x[1])] 
