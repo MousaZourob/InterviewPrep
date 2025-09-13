af8519ae205e8b0a10599ffc6f9aae7d290da73d
Mousa Zourob <mousazourob@gmail.com>
2025-09-13 14:45:30 -0400

    largestUniqueNumber

--- LC/Python/Easy/1133.py
+++ LC/Python/Easy/1133.py
@@ -1,9 +1,5 @@
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
         count = Counter(nums)
-        ans = -1
-        for k, v in count.items():
-            if k > ans and v == 1:
-                ans = k
-        
-        return ans
+
+        return max((k for k, v in count.items() if v == 1), default=-1)
