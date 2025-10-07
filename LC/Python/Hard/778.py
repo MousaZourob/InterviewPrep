19f7a00b31227e8dce85a4ac20660bba1f7bcd48
Mousa Zourob <mousazourob@gmail.com>
2025-10-06 21:54:15 -0400

    swimInWater

--- LC/Python/Hard/778.py
+++ LC/Python/Hard/778.py
@@ -1,24 +1,22 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        q = [(grid[0][0], 0, 0)]
         visited = [[0] * n for _ in range(n)]
         visited[0][0] = 1
-                
-        q = [(grid[0][0], 0, 0)]
-        
+
         while q:
-            elevation, r, c = heappop(q)
-            
+            t, r, c = heappop(q)
+
             if r == n - 1 and c == n - 1:
-                return elevation            
-            
-            for x, y in dirs:
-                n_r, n_c = r + x, c + y
-                
+                return t
+
+            for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                n_r = r + x
+                n_c = c + y
+
                 if 0 <= n_r < n and 0 <= n_c < n and not visited[n_r][n_c]:
-                    heappush(q, (max(elevation, grid[n_r][n_c]), n_r, n_c))
+                    heappush(q, (max(t, grid[n_r][n_c]), n_r, n_c))
                     visited[n_r][n_c] = 1
 
-                
         return -1
