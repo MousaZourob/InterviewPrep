a62bd4658a77a1e676870c73b4a4dcd0918394f0
Mousa Zourob <mousazourob@gmail.com>
2025-10-08 21:32:30 -0400

    successfulPairs

--- LC/Python/Medium/2300.py
+++ LC/Python/Medium/2300.py
@@ -1,1 +1,16 @@
-exako & 1600j 
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        sorted_spells = [(spell, index) for index, spell in enumerate(spells)]
+        sorted_spells.sort()
+        potions.sort()
+
+        ans = [0] * len(spells)
+        m = len(potions)
+        potion_i = m - 1
+        
+        for spell, i in sorted_spells:
+            while potion_i >= 0 and (spell * potions[potion_i]) >= success:
+                potion_i -= 1
+            ans[i] = m - (potion_i + 1)
+
+        return ans
