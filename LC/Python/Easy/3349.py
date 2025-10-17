298f12fff3dc25d1c98d5c9fc2a71b90ebd36bf0
Mousa Zourob <mousazourob@gmail.com>
2025-10-16 20:32:59 -0400

    hasIncreasingSubarrays

---
+++ LC/Python/Easy/3349.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        prev = 0
+        curr = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                curr += 1
+            else:
+                prev = curr
+                curr = 1
+            
+            if (prev >= k and curr >= k) or (curr >= 2 * k):
+                return True
+
+        return False
