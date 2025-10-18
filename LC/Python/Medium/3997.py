81055e5a996c97c678caf7e025fe771bed3d8257
Mousa Zourob <mousazourob@gmail.com>
2025-10-18 16:43:48 -0400

    maxDistinctElements

---
+++ LC/Python/Medium/3997.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxDistinctElements(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        ans = 0
+
+        prev = -inf
+
+        for num in nums:
+            curr = min(max(num - k, prev + 1), num + k)
+
+            if curr > prev:
+                ans += 1
+                prev = curr
+        
+        return ans
