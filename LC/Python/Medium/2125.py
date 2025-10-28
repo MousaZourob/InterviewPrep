a92d2348fe951dd4322c49c90219d87a71992dca
Mousa Zourob <mousazourob@gmail.com>
2025-10-27 20:08:35 -0400

    numberOfBeams

--- LC/Python/Medium/2125.py
+++ LC/Python/Medium/2125.py
@@ -1,13 +1,12 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         ans = 0
-        prev_count = 0
-        
+
+        prev = 0
         for row in bank:
-            row_count = row.count('1')
-            
-            if row_count > 0:
-                ans += prev_count * row_count
-                prev_count = row_count
-        
+            curr = row.count('1')
+            if curr > 0:
+                ans += prev * curr
+                prev = curr
+                
         return ans
