97585fa2f5e741140f145dd58e80e7b8134a3f8e
Mousa Zourob <mousazourob@gmail.com>
2025-11-18 11:48:20 -0500

    rob

--- LC/Python/Medium/198.py
+++ LC/Python/Medium/198.py
@@ -1,8 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob = not_rob = 0
-        
-        for n in nums:
-            rob, not_rob = max(not_rob + n, rob), rob
+        n = len(nums)
+        if n == 1:
+            return nums[0]
 
-        return rob
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2,n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        
+        return dp[-1]
