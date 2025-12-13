74fe4d7cb231cbb517e4ab8d194384b6c398d226
Mousa Zourob <mousazourob@gmail.com>
2025-12-13 14:22:42 -0500

    minProductSum

---
+++ LC/Python/Medium/1874.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort(reverse = True)
+        n = len(nums1)
+        ans = 0
+
+        for i in range(n):
+            ans += nums1[i] * nums2[i]
+
+        return ans
