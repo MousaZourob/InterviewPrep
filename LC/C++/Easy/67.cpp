a056d0ac001f4f5aaf44c646f8584638ab5e84ad
Mousa Zourob <mousazourob@gmail.com>
2026-01-19 11:07:53 -0500

    addBinary

---
+++ LC/C++/Easy/67.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int carry = 0;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            ans = char(sum % 2 + '0') + ans;
+            carry = sum / 2;
+        }
+
+        return ans;
+    }
+};
