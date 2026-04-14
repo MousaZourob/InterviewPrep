d2b346034a1b5a94c54cf5eae0f384b89cf73346
Mousa Zourob <mousazourob@gmail.com>
2026-04-13 21:10:52 -0400

    merge

---
+++ LC/C++/Medium/56.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans{intervals[0]};
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (ans.back()[1] >= intervals[i][0]) {
+                ans.back()[1] = std::max(ans.back()[1], intervals[i][1]);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+
+        return ans;
+    }
+};
