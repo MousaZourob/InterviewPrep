c2621b77e18c4d58a859f44a057dbfdce3690f1c
Mousa Zourob <mousazourob@gmail.com>
2026-04-20 09:23:37 -0400

    maxDistance

---
+++ LC/C++/Easy/2078.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        for(int i = 0; i <= colors.size()/2; i++) {
+            if(colors[0] != colors[colors.size()-1-i] || colors[colors.size()-1] != colors[i]) {
+                return colors.size()-i-1;
+            }
+        }
+        return 0;
+    }
+};
