5b6ac13b14c020cdd0daa1b14b0478b2a217347f
Mousa Zourob <mousazourob@gmail.com>
2026-05-12 19:27:36 -0400

    minimumEffort

---
+++ LC/C++/Hard/1665.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        std::sort(tasks.begin(), tasks.end(), 
+            [](const vector<int>& a, const vector<int>& b){
+                return (a[1] - a[0]) > (b[1] - b[0]);
+            });
+        
+        int ans = 0;
+        int spent = 0;
+
+        for (auto& task : tasks) {
+            ans = max(ans, spent + task[1]);
+            spent += task[0];
+        }
+
+        return ans;
+    }
+};
