a7861c129208cae929586f2e66672a56de7acee0
Mousa Zourob <mousazourob@gmail.com>
2026-06-04 23:11:34 -0400

    containsPattern

---
+++ LC/C++/Easy/1566.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsPattern(vector<int>& arr, int m, int k) {
+        int count = 0;
+
+        for (int i = 0; i + m < arr.size(); ++i) {
+            if (arr[i] == arr[i + m]) {
+                count++;
+                if (count == m * (k - 1)) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+};
