e592a4c58832ed861aea02be2b2503a31632d352
Mousa Zourob <mousazourob@gmail.com>
2026-08-04 22:52:20 -0400

    productExceptSelf

---
+++ LC/C++/Medium/238.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int pre = 1, post = 1;
+        int n = nums.size();
+        std::vector<int> ans(n, 1);
+
+        for (int i = 0; i < n; ++i) {
+            ans[i] = pre;
+            pre *= nums[i];
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] *= post;
+            post *= nums[i];
+        }
+
+        return ans;
+    }
+};
