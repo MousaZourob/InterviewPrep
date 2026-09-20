67568029c584ad796398cf87d0cb60da2fb13199
Mousa Zourob <mousazourob@gmail.com>
2026-09-20 16:15:49 -0400

    findBuildings

---
+++ LC/C++/Medium/1762.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        int maxSeen = heights.back();
+        std::vector<int> ans{};
+        ans.push_back(heights.size() - 1);
+
+        for (int i = heights.size() - 2; i >= 0; --i) {
+            if (heights[i] > maxSeen) {
+                ans.push_back(i);
+            }
+            maxSeen = std::max(maxSeen, heights[i]);
+        }
+        
+        for (int i = 0; i < ans.size() / 2; i++) {
+            int temp = ans[i];
+            ans[i] = ans[ans.size() - i - 1];
+            ans[ans.size() - i - 1] = temp; 
+        }
+        return ans;
+    }
+};
